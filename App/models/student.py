52a0af2162cde3fd94502142ac87cd00d52695d5
Kailash Joseph <kailashjoseph0@gmail.com>
2024-11-30 23:27:56 +0000

    "Added import for Notification model and implemented rank update notification logic in Student class"

--- App/models/student.py
+++ App/models/student.py
@@ -1,6 +1,7 @@
 from App.database import db
 from App.models import User
 from App.models.observer import Observer
+from App.models.notification import Notification
 
 class Student(User, Observer):
     __tablename__ = 'student'
@@ -61,12 +62,32 @@ class Student(User, Observer):
                 print(f"Team '{data['team']}' not found!")
         elif event == "RankUpdated":
             new_rank = data['curr']
+
+            if self.prev_rank == 0:
+                message = f'RANK : {new_rank}. Congratulations on your first rank!'
+            elif self.curr_rank == new_rank:
+                message = f'RANK : {self.curr_rank}. Well done! You retained your rank.'
+            elif self.curr_rank < new_rank:
+                message = f'RANK : {new_rank}. Congratulations! Your rank has went up.'
+            else:
+                message = f'RANK : {new_rank}. Oh no! Your rank has went down.'
+
+            # student.prev_rank = student.curr_rank
+            # ranking = Ranking(student.id, student.curr_rank, competition.date)
+            # notification = Notification(student.id, message)
+            # student.notifications.append(notification)
+
+
             if self.curr_rank != new_rank:
                 self.prev_rank = self.curr_rank
                 self.curr_rank = new_rank
+                notification = Notification(self.id, message)
+                self.notifications.append(notification)
                 # print("New rank is: ", new_rank)
                 #print("Inside notification:" + str(self.to_Dict()))
                 #print(f"StudentNotification: {self.username}, your rank has been updated to '{new_rank}'!")
+
+                
         else:
             print(f"Unknown event '{event}' occurred in team '{data.get('team', 'unknown')}'!")
 
